bbf8d2881a2d2dd7755e4c5d0a6eea531c54c282
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-01 12:39:13 +0300

    Creating the CampaignsModel.php model class

---
+++ application/models/CampaignsModel.php
@@ -0,0 +1,30 @@
+<?php namespace Models;
+
+/**
+ *This model handles all queries to the campaigns table.
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Models
+ *@package Models\Campaigns
+ */
+class CampaignsModel extends Model{
+	
+	/**
+	 *@var string The table name associated with this model
+	 */
+	protected $table = 'mailing_campaigns';
+
+	/**
+	 *This method creates a user log into the database
+	 *
+	 *@param ...
+	 *@return ...
+	 */
+	public static function create()
+	{
+		#...do something
+
+	}	
+
+
+}
