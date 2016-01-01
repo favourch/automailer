bc92e577517233921f1797e224d9b2a4d192bd25
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-01 15:36:39 +0300

    Writing the method for adding new mailing lists to the automailer application

--- application/controllers/ListsController.php
+++ application/controllers/ListsController.php
@@ -13,6 +13,8 @@
 
 use Drivers\Templates\View;
 use Models\ListsModel;
+use Helpers\Upload\Upload;
+use Helpers\Input\Input;
 
 class ListsController extends BaseController {
 
@@ -44,7 +46,34 @@ class ListsController extends BaseController {
 		$data['lists'] = ListsModel::all();
 
 		//get the ending date today
-		View::render('lists/home', $data);
+		View::render('lists/addnew', $data);
+
+	}
+
+	/**
+	*This method creats a mailing list in the database in the database
+	*@param null
+	*@return void
+	*/
+	public function postAddnew()
+	{
+		//upload the submitted file
+		$upload_file = Upload::doUpload('list_csv', 'application/storage/mailing_list_csvs');
+
+		//proceed if file upload success
+		if($upload_file->success)
+		{
+			//compose data to save in table
+			$list_info = array(
+				'name' => Input::get('name'),
+				'description' => Input::get('description'),
+				'csv_file_path' => $upload_file->upload_path_relative,
+			);
+
+			//call model to save this information into the database
+			$create_list = ListsModel::create($list_info);
+			
+		}
 
 	}
 	
