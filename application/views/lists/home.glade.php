f868c3fc573f0a960597b809b539e33bc8fd144d
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-01 12:37:00 +0300

    Creating the lists view file directory

---
+++ application/views/lists/home.glade.php
@@ -0,0 +1,62 @@
+@include('home/header')
+
+@include('home/menu')
+
+<!-- Simple Dashboard - START -->
+
+<!--main-->
+<div class="container" id="main">
+
+    <div class="row">
+
+        <div class="col-md-12 col-sm-12">
+
+            <div class="col-md-3"><a href="" class="btn btn-primary">Campaigns</a></div>
+            <div class="col-md-3"><a href="{{Url::link('lists')}}" class="btn btn-primary">Lists</a></div>
+            <div class="col-md-3"><a href="" class="btn btn-primary">Reports</a></div>
+            <div class="col-md-3"><a href="{{Url::link('lists/addnew')}}" class="btn btn-danger"><i class="fa fa-plus fa-lg"></i>List</a></div>
+
+        </div>
+
+        <div class="col-md-12 col-sm-12">
+
+            <table class="table table-hover table-striped">
+
+                <thead>
+                    <th>List Name</th><th>Size</th><th>Campaigns</th><th>Date Created</th><th></th>
+                </thead>
+
+                <tbody>
+
+                    @if(isset($lists) AND count($lists) > 0)
+
+                        @foreach($lists as $list)
+
+                            <tr>
+                                <td>{{$list['name']}}</td><td>{{$list['recepients']}}</td><td>{{$list['campaigns']}}</td><td>{{$list['date_created']}}</td><td><a href="">View Reports</a></td>
+                            </tr>
+
+                        @endforeach
+
+                    @else
+
+                        <tr>
+                            
+                           <p class="alert alert-info">The are no mailing lists yet...</p>
+
+                        </tr>
+
+                    @endif
+
+                </tbody>
+
+            </table>
+
+
+        </div>
+
+    </div>
+
+</div>
+
+@include('home/footer')
